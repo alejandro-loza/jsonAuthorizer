dea314cfc74758cc8d60bbefffa70224a56fe2a3
Pinky <alejandro.hernandez@finerio.mx>
2022-04-20 23:23:04 -0500

    Added comander

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Starting....");
+#! /usr/bin/env node
+const { Command } = require('commander');
+const program = new Command();
+
+program.command('authorize')
+  .description('Operations authorizer CLI from JSON file input')
+  .argument('<path>', 'the json file path')
+  .action((str, options) => {
+    console.log(str);
+    console.log(options);
+  });
+
+program.parse();
