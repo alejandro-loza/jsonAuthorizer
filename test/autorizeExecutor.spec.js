722d8ace3446ebe137981e077fbc75bd27ecafb4
Pinky <alejandro.hernandez@finerio.mx>
2022-04-26 13:18:46 -0500

    Added test to executor

---
+++ test/autorizeExecutor.spec.js
@@ -0,0 +1,36 @@
+import { executor } from '../src/commands/authorizeExecutor.js';
+import fs from 'fs';
+
+jest.mock('fs');
+
+
+describe('Should test the executor', ()=>{
+   it('Executor read, process and writes', async ()=> {
+      const readStream = {
+        pipe: jest.fn().mockReturnThis(),
+        on: jest.fn().mockImplementation(function (event, handler) {
+          handler();
+          return this;
+        }),
+      };
+
+      const writeStream = {
+        pipe: jest.fn().mockReturnThis(),
+        on: jest.fn().mockImplementation(function( event, handler) {
+          if (event === 'finish') {
+            handler();
+          }
+          return this;
+        }),
+      };
+
+      fs.createReadStream.mockReturnValueOnce(readStream);
+      fs.createWriteStream.mockReturnValueOnce(writeStream);
+
+      await executor('./files/input.json');
+      expect(fs.createReadStream).toBeCalledTimes(1);
+      expect(readStream.pipe).toBeCalledTimes(3);
+      expect(fs.createWriteStream).toBeCalledTimes(1);
+
+   });
+});
